9d96138d2265d6d92317d181538fc27ad91c7aa6
kaspar <kaspar.arme@gmail.com>
2017-10-20 12:50:32 +0300

    Update new report view; POST req and sum calculations not working as intended!

--- src/app/addreport/addreport.component.ts
+++ src/app/addreport/addreport.component.ts
@@ -2,10 +2,12 @@ import {Component, OnInit} from '@angular/core';
 import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
 import {Observable} from 'rxjs/Rx';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {DatePipe} from '@angular/common';
 import {NgIf} from '@angular/common';
-import {FormsModule, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormsModule, FormBuilder, FormGroup, Validators, FormArray} from '@angular/forms';
+import {Report, ExpenseDoc} from "../model/models";
+
 
 @Component({
   selector: 'app-root',
@@ -13,49 +15,122 @@ import {FormsModule, FormBuilder, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./addreport.component.css']
 })
 
-export class AddreportComponent {
-  rForm: FormGroup;
-  post: any;
-  issuer: string;
-  name: string;
-  startDate: string;
-  endDate: string;
-  content: string;
-  field: string;
-  project: string;
-  sum: string;
-  sumEur: string;
-  currency: string;
-  creditCard: boolean;
+export class AddreportComponent implements OnInit{
 
-  constructor(private fb: FormBuilder, private http: HttpClient) {
-    this.rForm = this.fb.group({
-      'name': [null, Validators.compose([Validators.required, Validators.minLength(1), Validators.maxLength(30)])],
-      'startDate': [null, Validators.compose([Validators.required, Validators.minLength(1), Validators.maxLength(30)])],
-      'endDate': [null, Validators.compose([Validators.required, Validators.minLength(1), Validators.maxLength(30)])],
-      'issuer': [null, Validators.compose([Validators.required, Validators.minLength(1), Validators.maxLength(30)])],
-      'content': [null, Validators.compose([Validators.required, Validators.minLength(1), Validators.maxLength(30)])],
-      'field': [null, Validators.compose([Validators.required, Validators.minLength(1), Validators.maxLength(30)])],
-      'project': [null, Validators.compose([Validators.required, Validators.minLength(1), Validators.maxLength(30)])],
-      'sum': [null, Validators.compose([Validators.required, Validators.minLength(1), Validators.maxLength(30)])],
-      'sumEur': [null, Validators.compose([Validators.required, Validators.minLength(1), Validators.maxLength(30)])],
-      'currency': [null, Validators.compose([Validators.required, Validators.minLength(1), Validators.maxLength(30)])],
-      'creditCard': [null],
-      // 'name': [null, Validators.compose([Validators.required, Validators.minLength(1), Validators.maxLength(30)])],
-      // 'validate': ''
+  public rForm: FormGroup;
+
+  constructor(private _fb: FormBuilder, private _http:HttpClient) { }
+
+  expenseSum: number = 0;
+  creditSum: number = 0;
+  totalSum: number = 0;
+  sum: number = 0;
+  rate: number = 1;
+  currencies = [
+    {name: "EEK", num:0.06},
+    {name: "EUR", num:1.00},
+    {name: "SEK", num:0.11},
+    {name: "USD", num:0.89},
+    {name: "LVL", num:1.00},
+    {name: "TRY", num:0.31},
+    {name: "LTL", num:1.00},
+    {name: "CHF", num:0.92},
+    {name: "RUB", num:0.01},
+    {name: "GBP", num:1.27}
+  ];
+
+  ngOnInit() {
+    this.rForm = this._fb.group({
+      employee: ['', [Validators.required]],
+      startDate: [''],
+      endDate: [''],
+      documents: this._fb.array([
+        this.initDocument(),
+      ])
     });
   }
 
-  onSubmit(data) {
-    console.log('form submitted', data);
-    const body = {name: 'Brad'};
+  initDocument() {
+    return this._fb.group({
+      date: [''],
+      issuer: ['', [Validators.required]],
+      content: ['', [Validators.required]],
+      field: ['', [Validators.required]],
+      project: ['', [Validators.required]],
+      sum: ['', [Validators.required]],
+      currency: ['EUR'],
+      creditCard: [false],
+      sumEur: [0]
+    })
+  }
 
-    this.http
-      .post('http://localhost:8080/reports', data)
-      // See below - subscribe() is still necessary when using post().
-      .subscribe(response => console.log(response));
-    // this.post.empolyee(this.rForm.value.categoryname).subscribe(data => {
-    // });
+  //Add new expense document to report
+  addDocument() {
+    const control = <FormArray>this.rForm.controls["documents"];
+    control.push(this.initDocument());
+  }
+
+  //Remove selected document from report
+  removeDocument(i: number) {
+    const control = <FormArray>this.rForm.controls["documents"];
+    control.removeAt(i);
+  }
+
+  newSum(event, i) {
+
+    let doc = this.rForm.controls["documents"].value[i];
+    doc.sum = event;
+    console.log("changed sum", doc);
+    this.update(i);
+  }
+
+  newCurrency(event, i) {
+    this.rate = this.currencies.filter(function(cur) {
+      return cur.name === event;
+    })[0].num;
+    console.log("changed currency", i);
+    this.update(i);
+  }
+
+  newCredit(event, i) {
+    let doc = this.rForm.controls["documents"].value[i];
+    doc.creditCard = event;
+    console.log("changed credit card", i);
+    this.update(i);
+  }
+
+  //Update sums when document sum, currency or creditcard values are changed
+  update(i) {
+    console.log("Starting update on", this);
+    let doc = this.rForm.controls["documents"].value[i];
+    doc.sumEur = +((doc.sum * this.rate).toFixed(2));
+    this.expenseSum = 0;
+    this.creditSum = 0;
+    this.totalSum = 0;
+    for (let document of this.rForm.controls["documents"].value) {
+      this.expenseSum += document.sumEur;
+      if(document.creditCard) {
+        this.creditSum += document.sumEur;
+      }
+      this.totalSum = +((this.expenseSum - this.creditSum).toFixed(2));
+    }
+    console.log("Updated", doc);
+  }
+
+  onSubmit(data) {
+    console.log('Form submitted:', JSON.stringify(data.value));
+
+    /*  //Returns 415 ERROR
+    this._http
+      .post('http://localhost:8080/reports', JSON.stringify( data.value ))
+      .subscribe();*/
+
+    /*   //Returns 403 ERROR
+    this._http
+      .post('http://localhost:8080/reports', JSON.stringify( data.value ), {
+        headers: new HttpHeaders().set('Content-Type', 'application/json')
+      })
+      .subscribe();*/
   }
 
 }
